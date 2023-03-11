59223192bf02a068f966aac9109f0fea9c8f4d93
David Moore <dwjmoore@gmail.com>
2023-03-11 17:07:31 +0000

    Refactored showDetails function. Used StringBuilder method for print statements.

--- Cart.java
+++ Cart.java
@@ -14,19 +14,15 @@ public class Cart {
 	}
 
 	public void showDetails() {
+		StringBuilder detailsSB = new StringBuilder();
 		if (items.isEmpty()) {
-			System.out.println("The cart is empty. Please add at least one product to see it in the cart.");
+			detailsSB.append(String.format("The cart is empty. Please add at least one product to see it in the cart.%n"));
 		} else {
-			System.out.println("--Cart--");
-			System.out.println("Item Count: " + (int) items.size());
-			System.out.println("Items:");
-			for (Product item : items) {
-				System.out.printf("%s: $%.2f%n", item.getName(), item.getPrice());
-			}
-			System.out.println();
-			System.out.printf("Pre-Tax Total: $%.2f%n", total);
-			System.out.printf("Post-Tax Total (%.2f%% Tax): $%.2f%n", taxRate * 100, total * (1 + taxRate));
+			appendCartHeaderToStringBuilder(detailsSB);
+			appendItemsWithPricesToStringBuilder(detailsSB);
+			appendCartTotalsToStringBuilder(detailsSB);
 		}
+		System.out.print(detailsSB);
 	}
 
 	public void checkout() {
@@ -37,5 +33,23 @@ public class Cart {
 			total = 0;
 			items.removeAll(items);
 		}
+	}
+
+	private void appendCartHeaderToStringBuilder(StringBuilder sb) {
+		sb.append(String.format("--Cart--%n"));
+		sb.append(String.format("Item Count: %d%n", items.size()));
+	}
+
+	private void appendItemsWithPricesToStringBuilder(StringBuilder sb) {
+		sb.append(String.format("Items:%n"));
+		for (Product item : items) {
+			sb.append(String.format("%s: $%.2f%n", item.getName(), item.getPrice()));
+		}
+		sb.append(String.format("%n"));
+	}
+
+	private void appendCartTotalsToStringBuilder(StringBuilder sb) {
+		sb.append(String.format("Pre-Tax Total: $%.2f%n", total));
+		sb.append(String.format("Post-Tax Total (%.2f%% Tax): $%.2f%n", taxRate * 100, total * (1 + taxRate)));
 	}
 }
